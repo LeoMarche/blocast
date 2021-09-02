8efd7737c6ca6dde13e299b19b5044991272f7f2
Léo Marché <leo.marche@polytechnique.edu>
2021-09-02 23:37:41 +0200

    added exchange Initialize list function and test

--- pkg/exchange/exchange.go
+++ pkg/exchange/exchange.go
@@ -1,5 +1,28 @@
 package exchange
 
+import "net"
+
+var DEFAULT_IPV6 = "[::1]"
+var DEFAULT_IPV4 = "127.0.0.1"
+
+var DEFAULT_SERVER_PORT = "23654"
+
+var CONN_LIST []net.Conn
+
+func InitializeList() error {
+	var conn net.Conn
+	var err error
+	conn, err = net.Dial("tcp", DEFAULT_IPV6+":"+DEFAULT_SERVER_PORT)
+	if err != nil {
+		conn, err = net.Dial("tcp", DEFAULT_IPV4+":"+DEFAULT_SERVER_PORT)
+		if err != nil {
+			return err
+		}
+	}
+	CONN_LIST = append(CONN_LIST, conn)
+	return nil
+}
+
 func Broadcast(b []byte) error {
 	//TODO implement the function to broadcast a message
 
---
+++ pkg/exchange/exchange_test.go
@@ -0,0 +1,55 @@
+package exchange
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitialize(t *testing.T) {
+
+	type test struct {
+		IPListen    string
+		IPType      int
+		IPServ      string
+		ShouldError bool
+		IsEmpty     bool
+	}
+
+	t1 := test{DEFAULT_IPV4, 4, DEFAULT_IPV4, false, false}
+	t2 := test{DEFAULT_IPV6, 6, DEFAULT_IPV6, false, false}
+	t3 := test{"10.10.10.10", 4, DEFAULT_IPV4, true, true}
+	t4 := test{"[::2]", 6, DEFAULT_IPV6, true, true}
+
+	tests := []test{t1, t2, t3, t4}
+
+	for _, v := range tests {
+		CONN_LIST = CONN_LIST[:0]
+
+		go func() {
+			if v.IPType == 4 {
+				DEFAULT_IPV4 = v.IPListen
+			} else if v.IPType == 6 {
+				DEFAULT_IPV6 = v.IPListen
+			}
+			err := InitializeList()
+			if v.ShouldError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		}()
+		l, err := net.Listen("tcp", v.IPServ+":"+DEFAULT_SERVER_PORT)
+		assert.NoError(t, err)
+		if v.IsEmpty {
+			assert.Empty(t, CONN_LIST)
+		} else {
+			conn, err := l.Accept()
+			assert.NoError(t, err)
+			assert.NotEmpty(t, CONN_LIST)
+			conn.Close()
+		}
+		l.Close()
+	}
+}
